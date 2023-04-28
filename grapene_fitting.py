fc27a87793550e214f153f6bf51aa2dc151a4359
TheHamer <66170230+TheHamer@users.noreply.github.com>
2023-04-28 13:54:44 +0100

    Finished code

--- grapene_fitting.py
+++ grapene_fitting.py
@@ -51,8 +51,6 @@ class RamanAnalysis(RamanData):
     def __init__(self, path):
         super().__init__(path)
 
-        self.cleaned_data = self.__clean_data(self.raman_data)
-
         self.peaks_all = []
         self.peaks_D = []
         self.peaks_G = []
@@ -280,30 +278,3 @@ class RamanAnalysis(RamanData):
         plt.xlabel("Peak mean / cm$^{-1}$")
         plt.ylabel("Count")
         plt.show()
-
-
-
-test_1 = RamanData(r'C:\Users\mr5014\Documents\PhD\VNA Code\Raman\Matt\graphene_03.txt')
-#test_1.plot_single_raman(23)
-
-test = RamanAnalysis(r'C:\Users\mr5014\Documents\PhD\VNA Code\Raman\Matt\graphene_03.txt')
-#result = test.fit_all_peaks()
-#print(result[0])
-#test.plot_D_mu()
-#test.plot_G_mu()
-#test.plot_2D_mu()
-#test.plot_all_fits()
-
-result_D = test.ratio_D_to_G(True)
-print(result_D)
-result_2D = test.ratio_2D_to_G(plot=True)
-print(result_2D)
-
-plt.hist([1.0740417533676154, 1.0142977907534785, 1.2021336944179075, 1.0616699076822835, 1.1220236567384883, 1.0960294081154351, 1.0203233454543794, 0.9527631925722418, 1.0852143062066526, 1.0739953675031368, 1.0127894144485532, 1.1221366990358161, 0.9979706818918804, 0.8861951483264627, 0.9112539608576421, 1.0221152591709335, 1.0100600018064867, 1.0382101049681882, 1.0441043322353558, 0.9891394841904894, 1.004276891920068, 1.1401236615761867, 0.9283584917162361, 1.1403288865876395], bins=30)
-plt.xlabel("Ratio")
-plt.ylabel("Count")
-plt.title(f"Ratio of D to G")
-plt.show()
-
-#print(test.raman_data)
-#test.plot_all_raman()
